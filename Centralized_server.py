9c4da111d3c5c7bfb575a80e949b35eac55158f5
Mohamed Elkhattam <medo20333882@yahoo.com>
2025-05-11 03:09:34 +0300

    Centralized Server Latest Changes

--- Centralized_server.py
+++ Centralized_server.py
@@ -1,18 +1,22 @@
+import hashlib
 import json
 import socket
 import threading
+import time
+from Distributed_hash_table import KEY_SPACE
 
 
 class CentralizedServer:
     def __init__(self):
         self.server_socket = None
         self.online_peers = []
+        self.DHT_nodes = {}
 
     def start_server(self):
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.server_socket.bind(('localhost', 8080))
         self.server_socket.listen()
-        print("Server Listening On Port 8080")
+        print("[Server] Listening on port 8080")
         while True:
             peer_connection, _ = self.server_socket.accept()
             server_thread = threading.Thread(
@@ -21,12 +25,17 @@ class CentralizedServer:
 
     def handle_incoming_peers(self, peer_connection):
         try:
-            peer_address = peer_connection.recv(1024).decode()
-            self.online_peers.append(peer_address)
+            peer_address = None
+            nodeAddress = None
+
             while True:
                 command = peer_connection.recv(1024).decode()
+                if command == "REGISTER_PEER":
+                    peer_address = peer_connection.recv(1024).decode()
+                    self.online_peers.append(peer_address)
+                    print("[Server] Registered peer with address", peer_address)
 
-                if command == "ONLINE_PEERS":
+                elif command == "ONLINE_PEERS":
                     online_peers_dict = {}
                     for i in range(len(self.online_peers)):
                         if self.online_peers[i] != peer_address:
@@ -39,8 +48,39 @@ class CentralizedServer:
                     print(f"[Server] {peer_address} disconnected.")
                     break
 
+                elif command == "SET_NODE_ID":
+                    nodeAddress = peer_connection.recv(1024).decode()
+                    if nodeAddress in self.DHT_nodes.values():
+                        nodeID = [
+                            id for id, address in self.DHT_nodes.items() if address == nodeAddress]
+                        peer_connection.send("YES".encode())
+                        time.sleep(0.01)
+                        peer_connection.send(str(nodeID[0]).encode())
+                    else:
+                        peer_connection.send("NO".encode())
+                        nodeID = self.getNodeID(nodeAddress)
+                        peer_connection.send(str(nodeID).encode())
+
+                elif command == "REMOVE_NODE":
+                    print(f"[Server] Removing node with address {nodeAddress}")
+                    if nodeAddress in self.DHT_nodes.values():
+                        nodeID = [
+                            id for id, address in self.DHT_nodes.items() if address == nodeAddress]
+                        self.DHT_nodes.pop(nodeID[0])
+
         except Exception as e:
             print(e)
+
+    def getNodeID(self, nodeAddress):
+        n = 0
+        while True:
+            salted = f"{nodeAddress}_{n}"
+            hash_bytes = hashlib.sha1(salted.encode()).digest()
+            id_4bit = int.from_bytes(hash_bytes, 'big') % KEY_SPACE
+            if id_4bit not in self.DHT_nodes.keys():
+                self.DHT_nodes[id_4bit] = nodeAddress
+                return id_4bit
+            n += 1
 
 
 if __name__ == "__main__":
